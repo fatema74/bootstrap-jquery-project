317c64d12d9f92382899bda6846974eae17a380d
fatema74 <fatema26721@gmail.com>
2025-02-16 22:58:54 +0600

    project setup complite

--- assete/js/app.js
+++ assete/js/app.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
   $(window).on("scroll", function () {
     var scroll = $(window).scrollTop();
-    console.log(scroll)
+    // console.log(scroll)
+
+    if (scroll >= 50) {
+      $(".sticky").addClass("stickyadd");
+    } else {
+      $(".sticky").removeClass("stickyadd")
+    }
   })
+
+  var typed = new typed('.element', {
+    string: ['Kanis Fatema', 'a Developer', 'a Designer'],
+    smartBackspace: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    startDelay: 1000
+  });
 })
